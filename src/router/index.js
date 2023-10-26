2ff36e2cdd20fec3783a92e580c3559dcb5ef71d
SUN <qiosion@gmail.com>
2023-10-26 09:33:51 +0900

    라우터 component 작성법 ver2

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
+import BlogHome from '@/views/BlogHome.vue';
 
 const routes = [
   {
@@ -12,7 +13,8 @@ const routes = [
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/BlogHome.vue'),
+        component: BlogHome,
+        // component: () => import(/* webpackChunkName: "home" */ '@/views/BlogHome.vue'),
       },
       {
         path: 'blog',
